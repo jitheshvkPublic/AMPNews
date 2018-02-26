152308811bb204b4a306c47af6bf43db8b2c8133
Jithesh Valsan <jithesh@whitecoat.com.au>
2018-02-26 16:31:07 +1100

    Added  extension to handle UI style.

--- AMPNews/CommonView/UiViewStyleExtension.swift
+++ AMPNews/CommonView/UiViewStyleExtension.swift
@@ -1,5 +1,5 @@
 //
-//  UiViewStyleExtension.swift
+//  UIViewStyleExtension.swift
 //  AMPNews
 //
 //  Created by Jithesh Valsan on 25/2/18.
@@ -13,35 +13,46 @@ extension UIView {
     func applyStyle(_ style:Style) {
         switch style {
         case .NewsArticleListHeaderLabelStyle:
-            guard let label = self as? UILabel else { return }
-            label.font = UIFont.preferredFont(forTextStyle: .headline)
-            label.textColor = UIColor.black
+            if let label = self as? UILabel {
+                label.font = UIFont.preferredFont(forTextStyle: .headline)
+                label.textColor = UIColor.black
+            }
         case .NewsArticleListAbstractLabelStyle:
-            guard let label = self as? UILabel else { return }
-            label.font = UIFont.preferredFont(forTextStyle: .footnote)
-            label.textColor = UIColor.black
+            if let label = self as? UILabel {
+                label.font = UIFont.preferredFont(forTextStyle: .footnote)
+                label.textColor = UIColor.black
+            }
         case .NewsArticleByLineLabelStyle:
-            guard let label = self as? UILabel else { return }
-            label.font = UIFont.boldSystemFont(ofSize: 12)
-            label.textColor = UIColor.lightGray
+            if let label = self as? UILabel {
+                label.font = UIFont.boldSystemFont(ofSize: 12)
+                label.textColor = UIColor.lightGray
+            }
         case .NewsArticleTimestampLabelStyle:
-            guard let label = self as? UILabel else { return }
-            label.font = UIFont.boldSystemFont(ofSize: 12)
-            label.textColor = UIColor.lightGray
+            if let label = self as? UILabel {
+                label.font = UIFont.boldSystemFont(ofSize: 12)
+                label.textColor = UIColor.lightGray
+            }
         case .NewsArticleRefreshControleStyle:
-            guard let refreshControle = self as? UIRefreshControl else { return }
-            refreshControle.attributedTitle = NSAttributedString(string: kNewStoriesRefreshControleText, attributes: [NSAttributedStringKey.foregroundColor : UIColor.black,NSAttributedStringKey.font:UIFont.boldSystemFont(ofSize: 12)])
-            refreshControle.tintColor = UIColor.black
+            if let refreshControle = self as? UIRefreshControl {
+                refreshControle.attributedTitle = NSAttributedString(string: kNewStoriesRefreshControleText, attributes: [NSAttributedStringKey.foregroundColor : UIColor.black,NSAttributedStringKey.font:UIFont.boldSystemFont(ofSize: 12)])
+                refreshControle.tintColor = UIColor.black
+            }
         case .NewsArticleTableViewCellStyle:
-            guard let tableViewCell = self as? UITableViewCell else { return }
-            tableViewCell.selectionStyle = .gray
+            if let tableViewCell = self as? UITableViewCell {
+                tableViewCell.selectionStyle = .gray
+            }
         case .NewsArticleTableViewStyle:
-            guard let tableView = self as? UITableView else { return }
-            tableView.rowHeight = UITableViewAutomaticDimension
-            tableView.estimatedRowHeight = 60
-            tableView.tableFooterView = UIView(frame: CGRect.zero)
-            tableView.separatorStyle = .singleLine
-            tableView.separatorInset = UIEdgeInsets.zero
+            if let tableView = self as? UITableView {
+                tableView.rowHeight = UITableViewAutomaticDimension
+                tableView.estimatedRowHeight = 60
+                tableView.tableFooterView = UIView(frame: CGRect.zero)
+                tableView.separatorStyle = .singleLine
+                tableView.separatorInset = UIEdgeInsets.zero
+            }
+        case .NewsArticleDetailsProgressViewStyle:
+            if let progressView = self as? UIProgressView {
+                progressView.progressTintColor = UIColor.blue
+            }
         }
     }
 }
